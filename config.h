1cb6b332bcb62f2142ae73672ab80639140b5a89
Madhuri Reddy <madhurireddy1311@gmail.com>
2023-04-27 14:23:02 -0400

    FINAL

--- config.h
+++ config.h
@@ -9,7 +9,7 @@ const char* id_name = "BeagleBoneB-HTTP";
 /* Enable Sensors */
 bool isEnable_VoltageIntern = true;
 bool isEnable_TemperatureExtern = true;	/*                     true: Enable  --  false: Disable                            */
-bool isEnable_Humidity = true;		/* If the sensor is disabled the data about it will not be displayed in the Tangle */
+bool isEnable_Humidity = true;		
 bool isEnable_Pressure = true;
 
 
--- main.c
+++ main.c
@@ -12,15 +12,15 @@ long count, init_t;
 
 int main ()
 {	
-    config(&dev);			/* Configure Name of the device, Endpoint address and Wifi credentials (if its a WiFi Connection), return string with the endpoint */	
-    initPeripherals(&count);	/* Init GPIOs, I2C and ADC */
-    connectNetwork(&dev, true);	/* Will drop out of this structure only when it has connected to the network and checks the existence of the endpoint */ 
+    config(&dev);			
+    initPeripherals(&count);	
+    connectNetwork(&dev, true);	
 
     while (1)
     {	
 	    init_t = take_time();
 
-	    pnp_sensors();		/* re init sensors to be able to plug n play in real time */
+	    pnp_sensors();		
 
 	    getData(&dev, &count);
 
